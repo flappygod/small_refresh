1feab56c279b7562ea2cbca1f04461346891a2ff
lilin <327603258@qq.com>
2025-04-27 19:10:43 +0800

    physic always

--- lib/src/small_refresh_physics.dart
+++ lib/src/small_refresh_physics.dart
@@ -1,33 +1,154 @@
+import 'dart:math' as math;
+
+import 'package:flutter/gestures.dart';
 import 'package:flutter/widgets.dart';
 
-/// 自定义 BouncingScrollPhysics，支持动态调整回弹边界
-class SmallRefreshBouncingScrollPhysics extends BouncingScrollPhysics {
-  /// 构造函数，支持动态切换模式
+class SmallRefreshBouncingScrollPhysics extends ScrollPhysics {
+  /// Creates scroll physics that bounce back from the edge.
   const SmallRefreshBouncingScrollPhysics({
+    this.decelerationRate = ScrollDecelerationRate.normal,
     super.parent,
-    super.decelerationRate,
   });
+
+  /// Used to determine parameters for friction simulations.
+  final ScrollDecelerationRate decelerationRate;
 
   @override
   SmallRefreshBouncingScrollPhysics applyTo(ScrollPhysics? ancestor) {
     return SmallRefreshBouncingScrollPhysics(
-      parent: buildParent(ancestor),
-    );
+        parent: buildParent(ancestor), decelerationRate: decelerationRate);
+  }
+
+  /// The multiple applied to overscroll to make it appear that scrolling past
+  /// the edge of the scrollable contents is harder than scrolling the list.
+  /// This is done by reducing the ratio of the scroll effect output vs the
+  /// scroll gesture input.
+  ///
+  /// This factor starts at 0.52 and progressively becomes harder to overscroll
+  /// as more of the area past the edge is dragged in (represented by an increasing
+  /// `overscrollFraction` which starts at 0 when there is no overscroll).
+  double frictionFactor(double overscrollFraction) {
+    return math.pow(1 - overscrollFraction, 2) *
+        switch (decelerationRate) {
+          ScrollDecelerationRate.fast => 0.26,
+          ScrollDecelerationRate.normal => 0.52,
+        };
   }
 
   @override
-  double applyBoundaryConditions(ScrollMetrics position, double value) {
-    //动态设置最小边界
-    final double minBoundary = 0;
-    //如果滚动超出最小边界
-    if (value < minBoundary) {
-      return value - minBoundary;
+  double applyPhysicsToUserOffset(ScrollMetrics position, double offset) {
+    assert(offset != 0.0);
+    assert(0 <= position.maxScrollExtent);
+
+    if (!position.outOfRange) {
+      return offset;
     }
-    //如果滚动超出最大边界（保持默认行为）
-    if (value > position.maxScrollExtent) {
-      return value - position.maxScrollExtent;
+
+    final double overscrollPastStart = math.max(0 - position.pixels, 0.0);
+    final double overscrollPastEnd =
+        math.max(position.pixels - position.maxScrollExtent, 0.0);
+    final double overscrollPast =
+        math.max(overscrollPastStart, overscrollPastEnd);
+    final bool easing = (overscrollPastStart > 0.0 && offset < 0.0) ||
+        (overscrollPastEnd > 0.0 && offset > 0.0);
+
+    final double friction = easing
+        // Apply less resistance when easing the overscroll vs tensioning.
+        ? frictionFactor(
+            (overscrollPast - offset.abs()) / position.viewportDimension)
+        : frictionFactor(overscrollPast / position.viewportDimension);
+    final double direction = offset.sign;
+
+    if (easing && decelerationRate == ScrollDecelerationRate.fast) {
+      return direction * offset.abs();
     }
-    //在合法范围内，不需要调整
-    return 0.0;
+    return direction * _applyFriction(overscrollPast, offset.abs(), friction);
+  }
+
+  static double _applyFriction(
+      double extentOutside, double absDelta, double gamma) {
+    assert(absDelta > 0);
+    double total = 0.0;
+    if (extentOutside > 0) {
+      final double deltaToLimit = extentOutside / gamma;
+      if (absDelta < deltaToLimit) {
+        return absDelta * gamma;
+      }
+      total += extentOutside;
+      absDelta -= deltaToLimit;
+    }
+    return total + absDelta;
+  }
+
+  @override
+  double applyBoundaryConditions(ScrollMetrics position, double value) => 0.0;
+
+  @override
+  Simulation? createBallisticSimulation(
+      ScrollMetrics position, double velocity) {
+    final Tolerance tolerance = toleranceFor(position);
+    if (velocity.abs() >= tolerance.velocity || position.outOfRange) {
+      return BouncingScrollSimulation(
+        spring: spring,
+        position: position.pixels,
+        velocity: velocity,
+        leadingExtent: 0,
+        trailingExtent: position.maxScrollExtent,
+        tolerance: tolerance,
+        constantDeceleration: switch (decelerationRate) {
+          ScrollDecelerationRate.fast => 1400,
+          ScrollDecelerationRate.normal => 0,
+        },
+      );
+    }
+    return null;
+  }
+
+  // The ballistic simulation here decelerates more slowly than the one for
+  // ClampingScrollPhysics so we require a more deliberate input gesture
+  // to trigger a fling.
+  @override
+  double get minFlingVelocity => kMinFlingVelocity * 2.0;
+
+  // Methodology:
+  // 1- Use https://github.com/flutter/platform_tests/tree/master/scroll_overlay to test with
+  //    Flutter and platform scroll views superimposed.
+  // 3- If the scrollables stopped overlapping at any moment, adjust the desired
+  //    output value of this function at that input speed.
+  // 4- Feed new input/output set into a power curve fitter. Change function
+  //    and repeat from 2.
+  // 5- Repeat from 2 with medium and slow flings.
+  /// Momentum build-up function that mimics iOS's scroll speed increase with repeated flings.
+  ///
+  /// The velocity of the last fling is not an important factor. Existing speed
+  /// and (related) time since last fling are factors for the velocity transfer
+  /// calculations.
+  @override
+  double carriedMomentum(double existingVelocity) {
+    return existingVelocity.sign *
+        math.min(0.000816 * math.pow(existingVelocity.abs(), 1.967).toDouble(),
+            40000.0);
+  }
+
+  // Eyeballed from observation to counter the effect of an unintended scroll
+  // from the natural motion of lifting the finger after a scroll.
+  @override
+  double get dragStartDistanceMotionThreshold => 3.5;
+
+  @override
+  double get maxFlingVelocity => switch (decelerationRate) {
+        ScrollDecelerationRate.fast => kMaxFlingVelocity * 8.0,
+        ScrollDecelerationRate.normal => super.maxFlingVelocity,
+      };
+
+  @override
+  SpringDescription get spring {
+    switch (decelerationRate) {
+      case ScrollDecelerationRate.fast:
+        return SpringDescription.withDampingRatio(
+            mass: 0.3, stiffness: 75.0, ratio: 1.3);
+      case ScrollDecelerationRate.normal:
+        return super.spring;
+    }
   }
 }
