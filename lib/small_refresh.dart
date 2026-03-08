c30ffa3dce526eca3504849e48fb680107191205
lijunlin <327603258@qq.com>
2026-03-08 15:46:20 +0800

    - Change SmallStickPageViewController just extends ScrollController.

--- lib/small_refresh.dart
+++ lib/small_refresh.dart
@@ -1,6 +1,7 @@
 library small_refresh;
 
 export 'src/small_stick_refresh_view.dart';
+export 'src/small_stick_controller.dart';
 export 'src/small_stick_page_view.dart';
 export 'src/small_refresh_footer.dart';
 export 'src/small_refresh_header.dart';
